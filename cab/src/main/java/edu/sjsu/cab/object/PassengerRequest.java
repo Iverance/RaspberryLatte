ed269a50e61a5e42c0849a8fa5a1e4fe361edd1e
briandmoon <brian.moon@sjsu.edu>
2015-03-07 10:05:50 -0800

    passenger request
    getters/setters/constructors

--- cab/src/main/java/edu/sjsu/cab/object/PassengerRequest.java
+++ cab/src/main/java/edu/sjsu/cab/object/PassengerRequest.java
@@ -9,6 +9,48 @@ public class PassengerRequest {
 	LatLng originLL;
 	LatLng destinationLL;
 	int numOfPassengers;
+	
+	public PassengerRequest(String originAddress, String destinationAddress,
+			LatLng originLL, LatLng destinationLL, int numOfPassengers) {
+		super();
+		this.originAddress = originAddress;
+		this.destinationAddress = destinationAddress;
+		this.originLL = originLL;
+		this.destinationLL = destinationLL;
+		this.numOfPassengers = numOfPassengers;
+	}
+	public String getOriginAddress() {
+		return originAddress;
+	}
+	public void setOriginAddress(String originAddress) {
+		this.originAddress = originAddress;
+	}
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+	public LatLng getOriginLL() {
+		return originLL;
+	}
+	public void setOriginLL(LatLng originLL) {
+		this.originLL = originLL;
+	}
+	public LatLng getDestinationLL() {
+		return destinationLL;
+	}
+	public void setDestinationLL(LatLng destinationLL) {
+		this.destinationLL = destinationLL;
+	}
+	public int getNumOfPassengers() {
+		return numOfPassengers;
+	}
+	public void setNumOfPassengers(int numOfPassengers) {
+		this.numOfPassengers = numOfPassengers;
+	}
+	
+	
 
 	
 }
